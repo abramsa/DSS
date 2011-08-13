7182d1eed5ab431299bccadeb1fd01a5b54b43a9
Austin Abrams <austin.abrams@gmail.com>
2011-08-13 16:17:45 -0700

    Added ability to send in your abstract.

--- dssapp/admin.py
+++ dssapp/admin.py
@@ -4,4 +4,4 @@ from django.contrib import admin
 admin.site.register(Advisor)
 admin.site.register(Student)
 admin.site.register(Event)
-admin.site.register(TalkPreference)
+admin.site.register(Talk)
