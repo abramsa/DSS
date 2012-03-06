f83319fe8f2e6b1eed1cc9d5c754779ade251622
Austin Abrams <austin.abrams@gmail.com>
2012-03-06 09:49:13 -0600

    Apparently Django 1.3 adds a required field to the file input field.

--- dssapp/views.py
+++ dssapp/views.py
@@ -318,7 +318,11 @@ def message(request):
     return render_to_response('dssapp/message.html', {'message': messages[msg_type]})
    
 class DSSVideoForm(forms.Form):
-    video = forms.FileField(required=True)    
+    def my_upload_directory(instance, filename):
+        extension = video.name[video.name.find('.'):]
+        return settings.VIDEO_ROOT + 'tmp' + extension
+        
+    video = forms.FileField(upload_to=my_upload_directory)    
  
 def upload(request):
     if not request.user.is_authenticated():
@@ -330,12 +334,9 @@ def upload(request):
             video = request.FILES['video']
 
             extension = video.name[video.name.find('.'):]
+            tmp_file = settings.VIDEO_ROOT + 'tmp' + extension
             file_name = settings.VIDEO_ROOT + talk.file_name() + extension
-
-            destination = open(file_name, 'wb+')
-            for chunk in video.chunks():
-                destination.write(chunk)
-            destination.close()
+            os.rename(tmp_file, file_name)
             
             return HttpResponseRedirect('schedule')
         else:
